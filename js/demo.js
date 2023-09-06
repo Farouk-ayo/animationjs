7dbc736331eee6f493e697baf9fba7ef97453d2f
Farouk-ayo <mustaphafarouk41@gmail.com>
2023-09-06 06:39:33 +0100

    done

--- js/demo.js
+++ js/demo.js
@@ -11,12 +11,12 @@ document.addEventListener(
 
     function initializeParticles() {
       particles = particleground(document.getElementById("particles"), {
-        dotColor: "#ffffff",
-        lineColor: "#a0c8e0",
-        minSpeedX: 0.01, // Adjust the speed values to be slower
-        maxSpeedX: 0.05, // You can experiment with different values
-        minSpeedY: 0.01, // to achieve the desired slow movement.
-        maxSpeedY: 0.05,
+        dotColor: "#ff0000",
+        lineColor: "#ff0000",
+        minSpeedX: 0.2, // Adjust the speed values to be slower
+        maxSpeedX: 0.3, // You can experiment with different values
+        minSpeedY: 0.2, // to achieve the desired slow movement.
+        maxSpeedY: 0.3,
         directionX: "center",
         directionY: "center",
         density: 2000,
@@ -28,17 +28,62 @@ document.addEventListener(
 
     initializeParticles();
 
+    //   const particleCon1 = document.querySelectorAll(".movement");
+    //   particleCon1.forEach((each) => {
+    //     each.addEventListener("mousemove", function () {
+    //       particles.options.minSpeedX = 0.3; // Adjust the speed values when the mouse is moved
+    //       particles.options.maxSpeedX = 0.7;
+    //       particles.options.minSpeedY = 0.3;
+    //       particles.options.maxSpeedY = 0.7;
+    //       particles.start();
+    //     });
+    //   });
+
+    //   const particleCon = document.querySelector(".part");
+    //   const particleCon2 = document.querySelector("#particles");
+
+    //   particleCon.addEventListener("mousemove", function () {
+    //     particles.pause();
+    //     particleCon2.style.overflow = "hidden";
+    //   });
+
+    //   particleCon2.addEventListener("mouseleave", function () {
+    //     particleCon2.style.overflow = "visible";
+    //     particles.start();
+    //   });
+    // },
+    // Mousemove event listeners for desktop
     const particleCon1 = document.querySelectorAll(".movement");
     particleCon1.forEach((each) => {
       each.addEventListener("mousemove", function () {
-        particles.options.minSpeedX = 0.3; // Adjust the speed values when the mouse is moved
-        particles.options.maxSpeedX = 0.7;
-        particles.options.minSpeedY = 0.3;
-        particles.options.maxSpeedY = 0.7;
+        adjustParticleSpeed();
         particles.start();
       });
     });
 
+    // Touchmove event listeners for mobile
+    particleCon1.forEach((each) => {
+      each.addEventListener("touchmove", function (event) {
+        // Get the touch position
+        const touch = event.touches[0];
+        const x = touch.clientX;
+        const y = touch.clientY;
+
+        // Adjust particle speed
+        adjustParticleSpeed();
+        particles.start();
+      });
+    });
+
+    // Function to adjust particle speed
+    function adjustParticleSpeed() {
+      particles.options.minSpeedX = 1.6;
+      particles.options.maxSpeedX = 1.7;
+      particles.options.minSpeedY = 1.6;
+      particles.options.maxSpeedY = 1.7;
+    }
+
+    // Mousemove event listener for pausing animation on desktop
     const particleCon = document.querySelector(".part");
     const particleCon2 = document.querySelector("#particles");
 
@@ -47,7 +92,20 @@ document.addEventListener(
       particleCon2.style.overflow = "hidden";
     });
 
+    // Touchmove event listener for pausing animation on mobile
+    particleCon.addEventListener("touchmove", function () {
+      particles.pause();
+      particleCon2.style.overflow = "hidden";
+    });
+
+    // Mouseleave event listener for resuming animation on desktop
     particleCon2.addEventListener("mouseleave", function () {
+      particleCon2.style.overflow = "visible";
+      particles.start();
+    });
+
+    // Touchend event listener for resuming animation on mobile
+    particleCon2.addEventListener("touchend", function () {
       particleCon2.style.overflow = "visible";
       particles.start();
     });
--- js/jquery.particleground.js
+++ js/jquery.particleground.js
@@ -459,11 +459,11 @@
     dotColor: "#666666",
     lineColor: "#666666",
     particleRadius: 7, // Dot size
-    lineWidth: 1,
+    lineWidth: 0.5,
     curvedLines: false,
-    proximity: 50, // How close two dots need to be before they join
+    proximity: 1, // How close two dots need to be before they join
     parallax: true,
-    parallaxMultiplier: 7, // The lower the number, the more extreme the parallax effect
+    parallaxMultiplier: 1, // The lower the number, the more extreme the parallax effect
     onInit: function () {},
     onDestroy: function () {},
   };
